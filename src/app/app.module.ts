8e0e0f6d73a1a79b642936d55a979f7d330f209f
Karan Kadam <karank@outlook.com>
2022-04-03 22:41:14 +0930

    added header and stories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoriesComponent } from './stories/stories.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoriesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
