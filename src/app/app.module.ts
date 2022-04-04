ff779fb796887ae8b115c9714daeff688cd67af9
Karan Kadam <karank@outlook.com>
2022-04-04 23:30:21 +0930

    feat: 🎸 ngx-moment pkg add for amTimeAgo, amFromUnix pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { MomentModule } from 'ngx-moment';
 
 // import hacker news api service
 import { HackernewsApiService } from './hackernews-api.service';
@@ -20,7 +21,7 @@ import { HackernewsApiService } from './hackernews-api.service';
     FooterComponent,
     ItemComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, MomentModule],
   providers: [HackernewsApiService],
   bootstrap: [AppComponent],
 })
